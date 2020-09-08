1d4bb06c898a23ff31e4063a3590b28648654f7e
ckrobinson83 <ckrobinson83@gmail.com>
2020-09-08 14:06:53 -0400

    Updated

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -11,7 +11,7 @@ namespace rpg.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        //who the fuck knows
+    
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
