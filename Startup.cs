879c7c711d94234f52a8b0840f78cad11dbc26f2
CKR 83 <ckrobinson83@gmail.com>
2020-09-10 11:14:59 -0400

    Test Upload to Git

--- Startup.cs
+++ Startup.cs
@@ -21,7 +21,7 @@ namespace rpg
         {
             Configuration = configuration;
         }
-
+        //just want to see if I can upload to git.
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
